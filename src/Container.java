9ce9f6578f5631bb1e87d66cc23ddd9763878efb
ndcroos <ndcroos@users.noreply.github.com>
2018-06-02 14:12:04 -0400

    Update Container.java

--- src/Container.java
+++ src/Container.java
@@ -6,13 +6,13 @@ import java.util.Set;
 
 public class Container implements IContainer{
 
-    private Map<ManagedInstance<Object>, Set<Class<?>>> context = new HashMap<>();
+    private Map<Embelished<Object>, Set<Class<?>>> context = new HashMap<>();
     
     @Override
-    public <T> ManagedInstance<T> instantiate(Class<T> clazz) throws Exception 
+    public <T> Embelished<T> instantiate(Class<T> clazz) throws Exception 
     {
         T newInstance = clazz.newInstance();
-        ManagedInstance<T> output = new ManagedInstance(newInstance);
+        Embelished<T> output = new Embelished(newInstance);
         return output;
     }
 
@@ -35,7 +35,7 @@ public class Container implements IContainer{
     @Override
     public void bind(Object instance, Class<?>... properties) {
         
-        ManagedInstance<Object> mInstance = new ManagedInstance(instance);
+        Embelishede<Object> mInstance = new Embelished(instance);
         
         Set<Class<?>> propSet = new HashSet<>();
         propSet.addAll(Arrays.asList(properties));
@@ -46,13 +46,13 @@ public class Container implements IContainer{
 
     @Override
     public void update(Object instance, Class<?>... properties) {
-        ManagedInstance<T> mInstance;
+        Embelished<T> mInstance;
         mInstance.notifyInstanceUpdated(instance, properties);
     }
 
     @Override
     public void unbind(Object instance) {
-        Set<Class<?>> get = this.context.get(new ManagedInstance(instance)); //equality?
+        Set<Class<?>> get = this.context.get(new Embelished(instance)); //equality?
         mInstance.notifyInstanceRemoved(instance);
     }
 }
