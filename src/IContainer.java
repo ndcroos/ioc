c70a3b194e0475c39d8ff4e33b841d197be18d98
ndcroos <ndcroos@users.noreply.github.com>
2018-06-02 13:54:11 -0400

    Update IContainer.java

--- src/IContainer.java
+++ src/IContainer.java
@@ -2,11 +2,11 @@ public interface IContainer {
 
     <T> ManagedInstance<T> instantiate(Class<T> clazz) throws Exception ;
     
-    Set<Object> find(Class<?>... properties);
+    Set<Object> find(Class<?>... props);
     
-    void bind(Object instance, Class<?>... properties);
+    void bind(Object instance, Class<?>... props);
     
-    void update(Object instance, Class<?>... properties);
+    void update(Object instance, Class<?>... props);
     
     void unbind(Object instance);
     
